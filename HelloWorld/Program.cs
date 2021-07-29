94f3bd5929009a5650b296175eaa1057da04aa80
Onur Oz <onureldrinx@gmail.com>
2021-07-30 01:29:37 +0300

    HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Your name:");
+            string name = Console.ReadLine();
+            Console.Write("Your Surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hello World!\nThis is "+name+" "+surname);
+        
+        }
+    }
+}
