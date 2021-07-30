b623ce2166a5b90fb5c8ca67096077a79f454b8b
Onur Oz <onureldrinx@gmail.com>
2021-07-30 22:18:17 +0300

    If-Elseif-Else

---
+++ Decisions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // If-Else
+
+            int time = DateTime.Now.Hour;
+
+            if(time < 12){
+
+                Console.WriteLine("Good Morning!");
+            
+            }else if(time < 18){
+
+                Console.WriteLine("Have a nice day!");
+
+            }else if(time < 23){
+
+                Console.WriteLine("Good Evening!");
+
+            }else{
+
+                Console.WriteLine("Good Night!");
+
+            }
+
+
+            string messageText = time <= 18 ? "Have a nice day!" : "Good evening!";
+            Console.WriteLine(messageText); 
+
+        }
+    }
+}
