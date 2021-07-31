198c56a35f65e77b55284c908101a569f42b97a5
Onur Oz <onureldrinx@gmail.com>
2021-08-01 01:13:02 +0300

    Number of Words in a Sentence

---
+++ Homework 1 - Algorithm Problems/Problem-4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+
+            Console.Write("Please, type a sentence    : ");
+            string sentence = Console.ReadLine().Trim();
+
+            string[] words;
+
+            words = sentence.Split(' ');
+
+
+
+            int wordCounter = 0;
+            int spaceCounter = 0;
+            int charCounter = 0;
+
+            foreach (var c in sentence)
+            {
+                
+
+
+                if(c.Equals(' ')){
+
+                    spaceCounter++;
+
+                }
+
+
+            }
+
+            wordCounter = spaceCounter+1;
+            charCounter = sentence.Length-spaceCounter;
+
+            Console.WriteLine("Total Number of Words      : " + words.Length);
+            Console.WriteLine("Total Number of Characters : " + charCounter);
+            Console.WriteLine("Total Number of Spaces     : " + spaceCounter);
+
+        }
+    }
+}
