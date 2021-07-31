daf81fbafbd4f61fb89bd1f3f51782aab3653964
Onur Oz <onureldrinx@gmail.com>
2021-07-31 22:27:14 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a=5;
+            int b=15;
+
+            Methods.print(a+" + "+b+" = "+Methods.sum(a,b));
+
+            // 'ref' keyword refers to the original address.
+            // In this way, the current method works on the original object instead of a copy of it.
+
+        }
+
+        
+    }
+
+    class Methods
+    {
+
+        public static void print(object o){
+
+            Console.WriteLine(o.ToString());
+
+        }
+
+        public static int sum(int a,int b){
+
+            return a+b;
+
+        } 
+
+
+
+
+    }
+
+
+}
