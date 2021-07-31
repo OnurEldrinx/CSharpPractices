92daa1bae353041ea86a51f8f087061ef365c2fa
Onur Oz <onureldrinx@gmail.com>
2021-07-31 18:18:58 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Initialize an Array
+
+            string[] colors = new string[5];
+
+            string[] animals = {"Cat","Dog","Bird","Cheetah","Lion"};
+
+            int[] numbers;
+
+            numbers = new int[10]; 
+
+            colors[0] = "Red";
+
+            numbers[9] = 9;
+
+            Console.WriteLine(colors[0] +" - "+animals[3]+" - "+numbers[9]);
+
+
+            // Loops and Arrays
+
+            Console.Write("Length of the Array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            int sum = 0;
+
+            for(int i=0;i<array.Length;i++){
+
+                Console.Write("Enter " + (i+1) + "." + "Number: ");
+                array[i] = int.Parse(Console.ReadLine());
+
+                sum = sum + array[i];
+
+            }
+
+            Console.WriteLine("Average: "+(sum/array.Length));
+
+        }
+    }
+}
